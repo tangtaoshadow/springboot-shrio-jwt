bffe437e6a3e2222e76daea25c03edc17ea1f208
tangtao <tangtao2099@outlook.com>
2019-07-19 21:05:57 +0800

    Initial commit

---
+++ src/main/java/com/tangtao/springbootshriojwt/SpringbootShrioJwtApplication.java
@@ -0,0 +1,13 @@
+package com.tangtao.springbootshriojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShrioJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShrioJwtApplication.class, args);
+    }
+
+}
